a48acbf1e1fa56e8b5c591628f5ebc5945cffc6b
AndropaX <kpax.mar@gmail.com>
2020-10-27 15:39:12 +0100

    zadanie1: minor changes

--- cw1/zadanie1.py
+++ cw1/zadanie1.py
@@ -1,9 +1,9 @@
-wiek1=float(input("Wpisz wiek studenta 1: "))
-wiek2=float(input("Wpisz wiek studenta 2: "))
+wiek1=int(input("Wpisz wiek studenta 1: "))
+wiek2=int(input("Wpisz wiek studenta 2: "))
 if wiek1>wiek2:
-    wynik='Student 1 jest starszy i ma '+str(int(wiek1))+' lat'
+    wynik='Student 1 jest starszy i ma '+str(wiek1)+' lat'
     print(wynik)
-    with open('cw1/wiek1.txt','a') as plik:
+    with open('cw1/wiek1.txt','w') as plik:
         plik.write(wynik)
 else:
     print('Student 1 nie jest starszy')
