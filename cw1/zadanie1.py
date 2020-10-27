0abb3c5b77953e0ba2ec637c474c7e1049d65885
AndropaX <kpax.mar@gmail.com>
2020-10-27 14:11:35 +0100

    zadanie1: simplify scrpit

--- cw1/zadanie1.py
+++ cw1/zadanie1.py
@@ -1,12 +1,9 @@
 wiek1=float(input("Wpisz wiek studenta 1: "))
 wiek2=float(input("Wpisz wiek studenta 2: "))
 if wiek1>wiek2:
-    wiekw=str(int(wiek1))
-    tekstp='Student 1 jest starszy i ma: '
-    tekstk=' lat'
-    print(tekstp,wiekw,tekstk)
-    wynik=tekstp,wiekw,tekstk
-    with open('wiek1.txt','a') as plik:
+    wynik='Student 1 jest starszy i ma '+str(int(wiek1))+' lat'
+    print(wynik)
+    with open('cw1/wiek1.txt','a') as plik:
         plik.write(''.join(wynik))
 else:
     print('Student 1 nie jest starszy')
