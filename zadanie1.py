2c4fea8643191a1c9ea5d64369e9c66ee835bb23
AndropaX <kpax.mar@gmail.com>
2020-10-27 13:30:04 +0100

    zadanie1: Add initial version with samle output

---
+++ zadanie1.py
@@ -0,0 +1,10 @@
+wiek1=float(input("Wpisz wiek studenta 1: "))
+wiek2=float(input("Wpisz wiek studenta 2: "))
+if wiek1>wiek2:
+    wiekw=str(int(wiek1))
+    tekstp='Student 1 jest starszy i ma: '
+    tekstk=' lat'
+print(tekstp,wiekw,tekstk)
+wynik=tekstp,wiekw,tekstk
+with open('wiek1.txt','a') as plik:
+    plik.write(''.join(wynik))
