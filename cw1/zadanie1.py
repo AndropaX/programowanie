f40bfa5b6192f54937794b5b31bf3e889a0277a5
AndropaX <kpax.mar@gmail.com>
2020-10-27 13:39:09 +0100

    zadanie1: fix script when if is not true

--- cw1/zadanie1.py
+++ cw1/zadanie1.py
@@ -4,7 +4,9 @@ if wiek1>wiek2:
     wiekw=str(int(wiek1))
     tekstp='Student 1 jest starszy i ma: '
     tekstk=' lat'
-print(tekstp,wiekw,tekstk)
-wynik=tekstp,wiekw,tekstk
-with open('wiek1.txt','a') as plik:
-    plik.write(''.join(wynik))
+    print(tekstp,wiekw,tekstk)
+    wynik=tekstp,wiekw,tekstk
+    with open('wiek1.txt','a') as plik:
+        plik.write(''.join(wynik))
+else:
+    print('Student 1 nie jest starszy')
