0708a53c2ec70cf2343bed7aa047d455e4d4b962
AndropaX <kpax.mar@gmail.com>
2020-11-10 12:15:09 +0100

    zadanie2: Add initial version

---
+++ cw2/zadanie2a.py
@@ -0,0 +1,19 @@
+import turtle
+bok=200
+predkosc=1
+tur=turtle.Pen()
+tur.speed(predkosc)
+tur.forward(bok)
+tur.left(90)
+tur.forward(bok)
+tur.left(90)
+tur.forward(bok)
+tur.left(90)
+tur.forward(bok)
+tur.forward(bok)
+tur.right(90)
+tur.forward(bok)
+tur.right(90)
+tur.forward(bok)
+tur.right(90)
+tur.forward(bok)
