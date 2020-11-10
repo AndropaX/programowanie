28625a0bfc16fed3203c8ae4b81b1150edfe8181
AndropaX <kpax.mar@gmail.com>
2020-11-10 12:14:51 +0100

    zadanie1: check if float is integer

--- cw2/zadanie1.py
+++ cw2/zadanie1.py
@@ -1,7 +1,11 @@
 zmienna=5.0
 if isinstance(zmienna,int)==True:
-    print("Zmienna jest wartością całkowitą")
+    print("Zmienna jest typu wartości całkowitej")
 if isinstance(zmienna,str)==True:
     print("Zmienna jest tekstem")
 if isinstance(zmienna,float)==True:
-    print("Zmienna jest wartością rzeczywistą")
+    print("Zmienna jest typu wartości rzeczywistej")
+    if zmienna.is_integer()==True:
+        print("Zmienna jest wartością całkowitą")
+    else:
+        print("Zmienna jest wartością rzeczywistą")
