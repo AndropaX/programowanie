bdabacf4a107f056f4fefc3282b910c19a92d977
AndropaX <kpax.mar@gmail.com>
2020-11-10 11:54:32 +0100

    cw2: zadanie1: Add initial version

---
+++ cw2/zadanie1.py
@@ -0,0 +1,7 @@
+zmienna=5.0
+if isinstance(zmienna,int)==True:
+    print("Zmienna jest wartością całkowitą")
+if isinstance(zmienna,str)==True:
+    print("Zmienna jest tekstem")
+if isinstance(zmienna,float)==True:
+    print("Zmienna jest wartością rzeczywistą")
