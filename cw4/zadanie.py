bde9727a238d2f62923cc565199dedefcbc8a25f
AndropaX <kpax.mar@gmail.com>
2020-12-10 10:01:22 +0100

    cw4: add time.sleep function

--- cw4/zadanie.py
+++ cw4/zadanie.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import glob
 import os
+import time
 
 lista=[]
 np.array(lista)
@@ -21,11 +22,14 @@ print("Stacja z minimalnym pomiarem temperatury:",tablica[minim,1][0][0])
 print("Godzina minimalnego pomiaru temperatury:",tablica[minim,3][0][0])
 max_temp=max(temperatura)
 maxim=np.where(temperatura==max_temp)
+time.sleep(2)
 print("Stacja z maksymalnym pomiarem temperatury:",tablica[maxim,1][0][0])
 print("Godzina maksymalnego pomiaru temperatury:",tablica[maxim,3][0][0])
 opad=tablica[:,8].astype(float)
 opad_t=np.where(0<opad)
 opad_l=list(opad_t[0])
+time.sleep(2)
 print("Stacje i godziny na których odnotowano opad:")
 for i in opad_l:
+    time.sleep(1)
     print(tablica[i,1],":",tablica[i,3])
