9cb4bac5c8c847940e326602b36aac9781f7e5f0
AndropaX <kpax.mar@gmail.com>
2021-01-20 19:23:15 +0100

    cw5: code cleanup, reorder and rename

--- cw5/zadanie.py
+++ cw5/zadanie.py
@@ -5,58 +5,52 @@ import requests
 import time
 from datetime import datetime
 from datetime import timedelta
-
-def newlist():
-    lista=[]
-    np.array(lista)
-    return lista
     
-def checkstation(number):
+def writeurl(url):
+    content=urllib.request.urlopen(url)
+    lines=[l.decode('utf-8') for l in content.readlines()]
+    data_list=[]
+    np.array(data_list)
+    data=csv.reader(lines)
+    next(data)
+    for row in data:
+        data_list.append(row)
+    return data_list
+
+def getdataurl(station_number):
     ok=0
     while ok==0:
-        url="https://danepubliczne.imgw.pl/api/data/synop/id/"+number+"/format/csv"
+        url="https://danepubliczne.imgw.pl/api/data/synop/id/"+station_number+"/format/csv"
         code=requests.get(url)
         if code.status_code==404:
             print("Niepoprawny kod stacji !")
-            number=input("Wpisz poprawny kod stacji: ")
+            station_number=input("Wpisz poprawny kod stacji: ")
         else:
             ok=1
     return url
-        
-def openurl(url):
-    cont=urllib.request.urlopen(url)
-    lines=[l.decode('utf-8') for l in cont.readlines()]
-    return lines
 
-def gettime(arg):
-    curr=datetime.now()
-    if arg==1:
-        time=curr
-    elif arg==2:
-        time=curr.strftime("%d.%m.%Y-%H.%M")
-    elif arg==3:
-        time=curr.strftime("%d/%m/%Y %H:%M")
-    elif arg==4:
-        date=curr.strftime("%d/%m/%Y/%H")
+def gettime(variant):
+    current=datetime.now()
+    if variant==1:
+        time=current
+    elif variant==2:
+        time=current.strftime("%d/%m/%Y %H:%M")
+    elif variant==3:
+        date=current.strftime("%d/%m/%Y/%H")
         time_zero=datetime.strptime(date,"%d/%m/%Y/%H")
         time=time_zero+timedelta(hours=1)
+    elif variant==4:
+        time=current.strftime("%d.%m.%Y-%H.%M")
     else:
         raise ValueError("Niepoprawny argument !")   
     return time
 
-def getsleeptime():
-    while gettime(4)<gettime(1):
-            time.sleep(1)
-    hourdiff=gettime(4)-gettime(1)
-    sleeptime=hourdiff.seconds+10
-    return sleeptime
-
-def convtime(time):
+def converttime(string):
     ok=0
     past=0
     while ok==0 or past==0:
         try:
-            if datetime.strptime(time,"%d/%m/%Y %H:%M")<gettime(1):
+            if datetime.strptime(string,"%d/%m/%Y %H:%M")<gettime(1):
                 print("Data lub czas jest przeszły !")
                 time=input("Wpisz poprawną datę i godzinę: ")
                 past=0
@@ -64,100 +58,87 @@ def convtime(time):
                 past=1
         except ValueError:
             print("Niepoprawny format daty i godziny !")
-            time=input("Wpisz poprawną datę i godzinę: ")
+            string=input("Wpisz poprawną datę i godzinę: ")
             ok=0
         else:
             ok=1
-    czas=datetime.strptime(time,"%d/%m/%Y %H:%M")
-    return czas
+    time=datetime.strptime(string,"%d/%m/%Y %H:%M")
+    return time
 
-def calcmeantemp(lista,inittime):
-    table=np.array(lista)
-    temp=np.mean(table[:,4].astype(float))
+def getsleeptime():
+    while gettime(3)<gettime(1):
+            time.sleep(1)
+    hour_difference=gettime(3)-gettime(1)
+    sleeptime=hour_difference.seconds+10
+    return sleeptime
+
+def saveresult(datalist,inittime):
+    table=np.array(datalist)
+    mean_temp=np.mean(table[:,4].astype(float))
     start_text=inittime.strftime("%d/%m/%Y %H:%M")
-    finish_text=gettime(3)
-    temp_text="Średnia temperatura od "+start_text+" do "+finish_text+", z "+str(len(table))+" pomiarów, ze stacji "+str(table[0,1])+" wynosiła: "+str(temp)+" st. C"
-    print(temp_text)
-    return temp_text
-
-def saveres(lista,inittime):
-    table=np.array(lista)
-    output=calcmeantemp(lista,inittime)
-    time=gettime(2)
-    filename="cw5/synop-"+time+".txt"
+    finish_text=gettime(2)
+    result_text="Średnia temperatura od "+start_text+" do "+finish_text+", z "+str(len(table))+" pomiarów, ze stacji "+str(table[0,1])+" wynosiła: "+str(mean_temp)+" st. C"
+    print(result_text)
+    filename_time=gettime(4)
+    filename="cw5/synop-"+filename_time+".txt"
     with open(filename,'w') as f:
         for i in table:
             f.write("%s\n" % i)
-        f.write(output)
+        f.write(result_text)
     print("Wyniki zostały zapisane do pliku:",filename)
     exit()
 
 # Odczytywanie i wyświetlanie listy stacji
-lista_st=newlist()
-lisurl=openurl("https://danepubliczne.imgw.pl/api/data/synop/format/csv")
-dane=csv.reader(lisurl)
-next(dane)
-for row in dane:
-    lista_st.append(row)
-tabl=np.array(lista_st)
+station_list=writeurl("https://danepubliczne.imgw.pl/api/data/synop/format/csv")
+station_table=np.array(station_list)
 print("Kod i nazwa stacji:")
-for i in tabl:
+for i in station_table:
    time.sleep(0.01)
    print(i[0],":",i[1])
 
 # Wybór stacji
-stacja=input("Wpisz kod stacji: ")
-requrl=checkstation(stacja)
-data=openurl(requrl)
+station_input=input("Wpisz kod stacji: ")
+request_url=getdataurl(station_input)
 
 # Określenie czasu działania
-print("Obecna data i godzina:",gettime(3))
-inlon=input("Wpisz datę i godzinę do której ma działać skrypt w formacie DD/MM/RRRR hh:mm : ")
-lon=convtime(inlon)
-init=gettime(1)
-cz_d=lon-init
-print("Czas działania skryptu:",cz_d)
+print("Obecna data i godzina:",gettime(2))
+time_input=input("Wpisz datę i godzinę do której ma działać skrypt w formacie DD/MM/RRRR hh:mm : ")
+target_time=converttime(time_input)
+init_time=gettime(1)
+working_time=target_time-init_time
+print("Czas działania skryptu:",working_time)
 
 #Zapisywanie pierwszej obserwacji
-lista_dan=newlist()
-data_dec=csv.reader(data)
-next(data_dec)
-for row in data_dec:
-    lista_dan.append(row)
+data_list=writeurl(request_url)
 
 #Zawieszenie działania do pełnej godziny
 sleeptime=getsleeptime()
-if cz_d.seconds<sleeptime:
+if working_time.seconds<sleeptime:
     print("Czas działania skryptu jest zbyt krótki, zostanie zapisana tylko jedna obserwacja")
-    saveres(lista_dan,init)
+    saveresult(data_list,init_time)
 else:
     print("Pobrano pierwszą obserwację, następna aktualizacja danych za:",int(sleeptime/60),"minut")
     time.sleep(sleeptime)
 
 #Następne zapytania
-while lon>gettime(1):
-    lista_new=newlist()
-    data2=openurl(requrl)
-    data_nd=csv.reader(data2)
-    next(data_nd)
-    for row in data_nd:
-        lista_new.append(row)
-    if lista_dan[-1][3]==lista_new[-1][3]:
+while target_time>gettime(1):
+    new_data=writeurl(request_url)
+    if data_list[-1][3]==new_data[-1][3]:
         print("Brak nowych danych, następna aktualizacja za: 5 minut")
         time.sleep(300)
     else:
-        new_sleep=getsleeptime()
-        rem_time=lon-gettime(1)
+        new_sleeptime=getsleeptime()
+        remaining_time=target_time-gettime(1)
         print("Pobrano nowe dane")
-        for i in lista_new:
-            lista_dan.append(i)
-        if new_sleep>rem_time.seconds:
+        for i in new_data:
+            data_list.append(i)
+        if new_sleeptime>remaining_time.seconds:
             print("Pozostały czas działania jest zbyt krótki, kończenie działania skryptu")
-            saveres(lista_dan,init)
+            saveresult(data_list,init_time)
         else:
-            print("Następna aktualizacja danych za:",int(new_sleep/60),"minut")
-            time.sleep(new_sleep)
+            print("Następna aktualizacja danych za:",int(new_sleeptime/60),"minut")
+            time.sleep(new_sleeptime)
 
 #Obliczanie średniej, zapisywanie wyników
 print("Czas działania skryptu dobiegł końca")
-saveres(lista_dan,init)
+saveresult(data_list,init_time)
